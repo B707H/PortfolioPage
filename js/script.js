838eb88f97bee199bb067f6482edb0e6431fb1e4
Bálint Tóth <tothbalint90@gmail.com>
2020-01-08 13:10:07 +0100

    feat(typed lib added): sub text gets typed

--- js/script.js
+++ js/script.js
@@ -5,5 +5,13 @@ $(document).ready(function() {
 		play: 5000,
 	});
 
+	var typed = new Typed(".typed", {
+		strings: ["Junior Software Engineer.", "Back-end Developer.", "Lifelong learner."],
+		typeSpeed: 70,
+		loop: true,
+		startDelay: 1000,
+		showCursor: false
+	});
+
 });
 
